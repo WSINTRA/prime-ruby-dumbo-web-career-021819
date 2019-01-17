2f32625a3bdd6d5ee1097cac591248370dbe421f
William Robert Simpson <will.simpson85@gmail.com>
2019-01-17 15:43:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,7 @@
 def prime?(num)
   if num % i == 0 && i < num || num < 0
     return false
-  (2..Math.sqrt(num)).each { |i|
-
-   end
-   }
+  end
+  (2..Math.sqrt(num)).each { |i| }
    true
  end
